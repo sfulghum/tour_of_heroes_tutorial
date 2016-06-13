fd1622b867dee0c040657974061d1fdf1d95f7e8
Steve Fulghum <steve@fulghum.digital>
2016-06-13 19:43:56 -0400

    Init

---
+++ app/hero.service.js
@@ -0,0 +1,12 @@
+"use strict";
+{
+    var HeroService = (function () {
+        function HeroService() {
+        }
+        HeroService.prototype.getHeroes = function () {
+        };
+        return HeroService;
+    }());
+    exports.HeroService = HeroService;
+}
+//# sourceMappingURL=hero.service.js.map
