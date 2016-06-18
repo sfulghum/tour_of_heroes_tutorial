9a3bdd38ed279bfd3ead6b3ee94d8d23252a7262
Steve Fulghum <steve@fulghum.digital>
2016-06-18 10:50:57 -0400

    Updates

--- app/hero.service.js
+++ app/hero.service.js
@@ -3,8 +3,6 @@
     var HeroService = (function () {
         function HeroService() {
         }
-        HeroService.prototype.getHeroes = function () {
-        };
         return HeroService;
     }());
     exports.HeroService = HeroService;
